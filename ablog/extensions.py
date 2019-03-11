079c0f80edb233702dec45cf53ee4b6270cafc48
chenggq <chengganqin@163.com>
2019-03-11 11:02:40 +0800

    update

--- ablog/extensions.py
+++ ablog/extensions.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_migrate import Migrate
-from celery import Celery
+# from celery import Celery
 
 
 bootstrap = Bootstrap()
@@ -19,7 +19,7 @@ mail = Mail()
 moment = Moment()
 toolbar = DebugToolbarExtension()
 migrate = Migrate()
-celery_app = Celery()
+# celery_app = Celery()
 
 
 @login_manager.user_loader
